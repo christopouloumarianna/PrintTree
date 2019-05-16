7fb4af141c1a9091ed685a8abe96a66b12244deb
christopouloumarianna <50484642+christopouloumarianna@users.noreply.github.com>
2019-05-16 22:17:18 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace PrintTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter symbol");
+
+            char symbol = char.Parse(Console.ReadLine());
+
+            PrintTree(4, symbol);
+
+            Console.ReadLine();
+
+        }
+
+
+
+        static void PrintTree(int height, char symbol)
+        {
+
+            int n = height;
+
+            int t = 1;
+            for (int i = 0; i < n; i++)
+
+            {
+                for (int z = height - i; z > 0; z--)
+
+                {
+
+                    Console.Write(" ");
+
+                }
+
+                for (int j = 0; j < 2 * t - 1; j++)
+
+                {
+                    Console.Write(symbol);
+
+                }
+
+                t++;
+
+                Console.Write("\n");
+
+            }
+        }
+    }
+}
+
